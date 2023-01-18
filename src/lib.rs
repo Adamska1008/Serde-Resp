99deb5c9730eb2380da1ab4a700c0d75c01c98d8
Adamska1008 <2639980868@qq.com>
2023-01-18 21:21:52 +0800

    finish ser part, pass all test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
 pub mod de;
 pub mod error;
 pub mod ser;
+
+pub use crate::error::{Error, Result};
+
+pub mod resp_type {
+    enum RESPType {
+
+    }
+}
--- src/ser.rs
+++ src/ser.rs
@@ -1,6 +1,7 @@
 use serde::{ser, Serialize};
 use serde::ser::Impossible;
 use crate::error::{Error, Result};
+use crate::error::Error::UnexpectedType;
 
 pub struct Serializer {
     output: String
@@ -19,9 +20,9 @@ where T: Serialize
 impl<'a> ser::Serializer for &'a mut Serializer {
     type Ok = ();
     type Error = Error;
-    type SerializeSeq = Impossible<(), Error>;
-    type SerializeTuple = Impossible<(), Error>;
-    type SerializeTupleStruct = Impossible<(), Error>;
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
     type SerializeTupleVariant = Impossible<(), Error>;
     type SerializeMap = Impossible<(), Error>;
     type SerializeStruct = Impossible<(), Error>;
@@ -96,61 +97,158 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     }
 
     fn serialize_none(self) -> Result<()> {
+        self.serialize_unit()
+    }
+
+    fn serialize_some<T: ?Sized>(self, value: &T) -> Result<()> where T: Serialize {
+        value.serialize(self)
+    }
+
+
+    fn serialize_unit(self) -> Result<()> {
         self.output += "$-1\r\n";
         Ok(())
     }
 
-    fn serialize_some<T: ?Sized>(self, value: &T) -> Result<()> where T: Serialize {
-        todo!()
+    fn serialize_unit_struct(self, _: &'static str) -> Result<()> {
+        Err(UnexpectedType)
     }
 
-    fn serialize_unit(self) -> Result<()> {
-        todo!()
+    fn serialize_unit_variant(self, _: &'static str, _: u32, _: &'static str) -> Result<()> {
+        Err(UnexpectedType)
     }
 
-    fn serialize_unit_struct(self, name: &'static str) -> Result<()> {
-        todo!()
+    fn serialize_newtype_struct<T: ?Sized>(self, _: &'static str, _: &T) -> Result<()> where T: Serialize {
+        Err(UnexpectedType)
     }
 
-    fn serialize_unit_variant(self, name: &'static str, variant_index: u32, variant: &'static str) -> Result<()> {
-        todo!()
-    }
-
-    fn serialize_newtype_struct<T: ?Sized>(self, name: &'static str, value: &T) -> Result<()> where T: Serialize {
-        todo!()
-    }
-
-    fn serialize_newtype_variant<T: ?Sized>(self, name: &'static str, variant_index: u32, variant: &'static str, value: &T) -> Result<()>
+    fn serialize_newtype_variant<T: ?Sized>(self, _: &'static str, _: u32, _: &'static str, _: &T) -> Result<()>
         where T: Serialize
     {
-        todo!()
+        Err(UnexpectedType)
     }
 
-    fn serialize_seq(self, len: Option<usize>) -> std::result::Result<Self::SerializeSeq, Self::Error> {
-        todo!()
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq> {
+        match len {
+            Some(x) => self.output += &format!("*{x}\r\n"),
+            None => self.output += &format!("*0\r\n")
+        }
+        Ok(self)
     }
 
-    fn serialize_tuple(self, len: usize) -> std::result::Result<Self::SerializeTuple, Self::Error> {
-        todo!()
+    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple> {
+        self.serialize_seq(Some(len))
     }
 
-    fn serialize_tuple_struct(self, name: &'static str, len: usize) -> std::result::Result<Self::SerializeTupleStruct, Self::Error> {
-        todo!()
+    fn serialize_tuple_struct(self, _: &'static str, len: usize) -> Result<Self::SerializeTupleStruct> {
+        self.serialize_seq(Some(len))
     }
 
-    fn serialize_tuple_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> std::result::Result<Self::SerializeTupleVariant, Self::Error> {
-        todo!()
+    fn serialize_tuple_variant(self, _: &'static str, _: u32, _: &'static str, _: usize) -> Result<Self::SerializeTupleVariant> {
+        Err(UnexpectedType)
     }
 
-    fn serialize_map(self, len: Option<usize>) -> std::result::Result<Self::SerializeMap, Self::Error> {
-        todo!()
+    fn serialize_map(self, _: Option<usize>) -> Result<Self::SerializeMap> {
+        Err(UnexpectedType)
     }
 
-    fn serialize_struct(self, name: &'static str, len: usize) -> std::result::Result<Self::SerializeStruct, Self::Error> {
-        todo!()
+    fn serialize_struct(self, _: &'static str, _: usize) -> Result<Self::SerializeStruct> {
+        Err(UnexpectedType)
     }
 
-    fn serialize_struct_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> std::result::Result<Self::SerializeStructVariant, Self::Error> {
-        todo!()
+    fn serialize_struct_variant(self, _: &'static str, _: u32, _: &'static str, _: usize) -> Result<Self::SerializeStructVariant> {
+        Err(UnexpectedType)
+    }
+}
+
+impl<'a> ser::SerializeSeq for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<()>
+    where T: Serialize {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeTuple for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<()>
+    where T: Serialize {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+impl<'a> ser::SerializeTupleStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<()>
+    where T: Serialize {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod serializer_test {
+    use crate::error::Result;
+    use crate::ser;
+
+    #[test]
+    fn test_string() -> Result<()> {
+        let str = "Hello, world!";
+        let expected = format!("${}\r\n{}\r\n", str.len(), str);
+        assert_eq!(ser::to_string(&str)?, expected);
+        let str = str.to_owned();
+        assert_eq!(ser::to_string(&str)?, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_number() -> Result<()>{
+        let number = 114514;
+        let expected = ":114514\r\n";
+        assert_eq!(ser::to_string(&number)?, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_null() -> Result<()> {
+        let expected = "$-1\r\n";
+        let null: Option<&str> = None;
+        assert_eq!(ser::to_string(&null)?, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_array() -> Result<()> {
+        let array = [1, 2, 3, 4, 5];
+        let expected = "*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n:5\r\n";
+        assert_eq!(ser::to_string(&array)?, expected);
+        let array = ["hello", "world"];
+        let expected = "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n";
+        assert_eq!(ser::to_string(&array)?,expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_tuple() -> Result<()> {
+        let tuple = (32, 7, "abcd");
+        let expected = "*3\r\n:32\r\n:7\r\n$4\r\nabcd\r\n";
+        assert_eq!(ser::to_string(&tuple)?, expected);
+        Ok(())
     }
 }
